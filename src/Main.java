b227d80b96bf35d06e7bcb28c12e7ee06a158e96
Akash4O <akashkolde1320@gmail.com>
2024-12-14 22:20:27 +0530

    Structurized Shell

--- src/Main.java
+++ src/Main.java
@@ -9,157 +9,157 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
+    private static String pwd = Paths.get("").toAbsolutePath().toString();
+    private static final String[] commands = {"echo", "exit", "type", "pwd", "cd", "cat", "ls"};
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        String input, typeSubString;
-        String[] commands = {"echo", "exit", "type", "pwd", "cd" , "cat" , "ls"};
-        String pwd = Paths.get("").toAbsolutePath().toString();
-
+        System.out.println("Welcome to POSIX-Shell. Type 'exit 0' to quit.");
         while (true) {
             System.out.print("$ ");
-            input = scanner.nextLine();
+            String input = scanner.nextLine();
 
             if (input.equals("exit 0")) {
                 break;
             } else if (input.startsWith("echo")) {
-                String content = input.substring(4).trim();
-
-                if (content.startsWith("\"")) {
-                    Pattern pattern = Pattern.compile("\"(.*?)\"");
-                    Matcher matcher = pattern.matcher(content);
-
-                    StringBuilder concatenatedContent = new StringBuilder();
-                    int count = 0;
-                    while (matcher.find()) {
-                        String match = matcher.group(1);
-
-                        
-                        
-                        if (count>0) {
-                            concatenatedContent.append(" "+match);
-                        }else{
-                            concatenatedContent.append(match);
-                            count++;
-                        }
-                    }
-                content = concatenatedContent.toString();
-                }else
-                if ((content.startsWith("'") && content.endsWith("'")) || (content.startsWith("\"") && content.endsWith("\""))) {
-                    content = content.substring(1, content.length() - 1);
-                } else {
-                    content = content.replaceAll("\\s+", " ");
-                }
-                System.out.println(content);
+                handleEcho(input);
             } else if (input.startsWith("type")) {
-                typeSubString = input.substring(5);
-                if (Arrays.asList(commands).contains(typeSubString)) {
-                    System.out.println(typeSubString + " is a shell builtin");
-                } else {
-                    String path = getPath(typeSubString.trim());
-                    if (path != null) {
-                        System.out.println(typeSubString + " is " + path);
-                    } else {
-                        System.out.println(typeSubString + ": not found");
-                    }
-                }
+                handleType(input);
             } else if (input.equals("pwd")) {
-                System.out.println(pwd);
+                handlePwd();
             } else if (input.startsWith("cd")) {
-                String dir = input.substring(3).trim();
-
-                if (dir.startsWith("./")) {
-                    String cdir = input.substring(4);
-                    dir = pwd + cdir;
-                    if (Files.isDirectory(Path.of(dir)) && Files.exists(Path.of(dir))) {
-                        pwd = dir;
-                    } else {
-                        System.out.printf("cd: %s: No such file or directory%n", dir);
-                    }
-
-                } else if (dir.startsWith("../")) {
-                    Path newPath = Path.of(pwd).resolve(dir).normalize();
-                    if (Files.isDirectory(Path.of(dir)) && Files.exists(Path.of(dir))) {
-                        pwd = newPath.toString();
-                    } else {
-                        System.out.printf("cd: %s: No such file or directory%n", dir);
-                    }
-                } else if (dir.equals("..")) {
-                    Path parentPath = Path.of(pwd).getParent();
-                    if (parentPath != null) {
-                        pwd = parentPath.toString();
-                    }
-                } else if (dir.equals("~")) {
-                    String userHome = System.getProperty("user.home");
-                    if (System.getenv("HOME") != null) {
-                        userHome = System.getenv("HOME");
-                    }
-                    pwd = userHome;
-                } else {
-                    if (Files.isDirectory(Path.of(dir)) && Files.exists(Path.of(dir))) {
-                        pwd = dir;
-                    } else {
-                        System.out.printf("cd: %s: No such file or directory%n", dir);
-                    }
-                }
-
+                handleCd(input);
             } else if (input.startsWith("cat")) {
-                String[] argsArray = input.substring(4).trim().split(" ");
-                for (String fileName : argsArray) {
-                    if ((fileName.startsWith("'") && fileName.endsWith("'") )||(fileName.startsWith("\"")&&fileName.endsWith("\""))) {
-                        fileName = fileName.substring(1, fileName.length() - 1);
-                    }
-                    Path filePath = Paths.get(pwd, fileName.trim());
-
-                    if (Files.exists(filePath)) {
-                        try {
-                            System.out.println("\n\n"+fileName+": \n\n");
-                            Files.lines(filePath).forEach(System.out::println);
-                        } catch (IOException e) {
-                            System.out.println("cat: " + fileName + ": Error reading file");
-                        }
-                    } else {
-                        System.out.println("cat: " + fileName + ": No such file");
-                    }
-                }
-                
-            }else if (input.startsWith("ls")||input.equals("ls")) {
-                File directory = new File(pwd);
-                File[] files = directory.listFiles();
-                int count = 0;
-                if (files!=null) {
-                    for(File file : files){
-                        if (file.isDirectory()) {
-                            System.out.printf("/"+file.getName()+"\t");    
-                        }else{
-                            System.out.printf(file.getName()+"\t\t");
-                        }
-                        count++;
-                        if (count%4==0) {
-                            System.out.println();
-                            System.out.println();
-                        }
-                        
-                    }
-                }
-                System.out.println();
+                handleCat(input);
+            } else if (input.startsWith("ls") || input.equals("ls")) {
+                handleLs();
+            } else {
+                handleExternalCommand(input);
             }
-            
-            else {
-                String command = input.split(" ")[0];
+        }
+        scanner.close();
+    }
 
-                String path = getPath(command);
+    private static void handleEcho(String input) {
+        String content = input.substring(4).trim();
 
-                if (path == null) {
-                    System.out.printf("%s: command not found%n", command);
+        if (content.startsWith("\"")) {
+            Pattern pattern = Pattern.compile("\"(.*?)\"");
+            Matcher matcher = pattern.matcher(content);
+            StringBuilder concatenatedContent = new StringBuilder();
+            int count = 0;
+
+            while (matcher.find()) {
+                String match = matcher.group(1);
+                if (count > 0) {
+                    concatenatedContent.append(" ").append(match);
                 } else {
-                    String fullPath = path + input.substring(command.length());
-
-                    Process p = Runtime.getRuntime().exec(fullPath.split(" "));
-
-                    p.getInputStream().transferTo(System.out);
+                    concatenatedContent.append(match);
+                    count++;
                 }
             }
+            content = concatenatedContent.toString();
+        } else if ((content.startsWith("'") && content.endsWith("'")) || (content.startsWith("\"") && content.endsWith("\""))) {
+            content = content.substring(1, content.length() - 1);
+        } else {
+            content = content.replaceAll("\\s+", " ");
+        }
+        System.out.println(content);
+    }
 
+    private static void handleType(String input) {
+        String typeSubString = input.substring(5).trim();
+        if (Arrays.asList(commands).contains(typeSubString)) {
+            System.out.println(typeSubString + " is a shell builtin");
+        } else {
+            String path = getPath(typeSubString);
+            if (path != null) {
+                System.out.println(typeSubString + " is " + path);
+            } else {
+                System.out.println(typeSubString + ": not found");
+            }
+        }
+    }
+
+    private static void handlePwd() {
+        System.out.println(pwd);
+    }
+
+    private static void handleCd(String input) {
+        String dir = input.substring(3).trim();
+
+        try {
+            if (dir.startsWith("./")) {
+                dir = pwd + "/" + dir.substring(2);
+            } else if (dir.startsWith("../")) {
+                dir = Path.of(pwd).resolve(dir).normalize().toString();
+            } else if (dir.equals("..")) {
+                Path parentPath = Path.of(pwd).getParent();
+                if (parentPath != null) pwd = parentPath.toString();
+                return;
+            } else if (dir.equals("~")) {
+                pwd = System.getProperty("user.home");
+                return;
+            }
+
+            if (Files.isDirectory(Path.of(dir)) && Files.exists(Path.of(dir))) {
+                pwd = dir;
+            } else {
+                System.out.printf("cd: %s: No such file or directory%n", dir);
+            }
+        } catch (Exception e) {
+            System.out.printf("cd: %s: Error occurred%n", dir);
+        }
+    }
+
+    private static void handleCat(String input) {
+        String[] argsArray = input.substring(4).trim().split(" ");
+        for (String fileName : argsArray) {
+            if ((fileName.startsWith("'") && fileName.endsWith("'")) || (fileName.startsWith("\"") && fileName.endsWith("\""))) {
+                fileName = fileName.substring(1, fileName.length() - 1);
+            }
+            Path filePath = Paths.get(pwd, fileName.trim());
+
+            if (Files.exists(filePath)) {
+                try {
+                    System.out.println("\n\n" + fileName + ": \n\n");
+                    Files.lines(filePath).forEach(System.out::println);
+                } catch (IOException e) {
+                    System.out.println("cat: " + fileName + ": Error reading file");
+                }
+            } else {
+                System.out.println("cat: " + fileName + ": No such file");
+            }
+        }
+    }
+
+    private static void handleLs() {
+        File directory = new File(pwd);
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    System.out.printf("/%s\n\n", file.getName());
+                } else {
+                    System.out.printf("%s\n\n", file.getName());
+                }
+
+                
+            }
+        }
+    }
+
+    private static void handleExternalCommand(String input) throws IOException {
+        String command = input.split(" ")[0];
+        String path = getPath(command);
+
+        if (path == null) {
+            System.out.printf("%s: command not found%n", command);
+        } else {
+            String fullPath = path + input.substring(command.length());
+            Process p = Runtime.getRuntime().exec(fullPath.split(" "));
+            p.getInputStream().transferTo(System.out);
         }
     }
 
